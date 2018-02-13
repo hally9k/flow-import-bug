a798339f4b19fdb0d2abb1976f663b6462440325
hally9k <hally9k@gmail.com>
2018-02-13 18:53:27 +1300

    Renamed files

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // @flow
+//
+// Run `flow` and see the type checking fail correctly because of the missing name property.
+// To reproduce the bug: Comment out the import statement below.
+// The incorrect Person type below will pass type checking incorrectly.
 import { type Person } from './base.js.flow'
 
-// This fails type checking (correctly) when the type is explicitly exported above
-// Comment it out and see everything pass type checking (incorrectly)
 const alexis: Person = {
     randomKeyInsteadOfName: 'Alexis Sanchez'
 }
 
 const ManchesterUnited: Team = {
-    players: [alexis]
+    players: [alexis, lukaku]
 }
